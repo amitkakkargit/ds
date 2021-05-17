abd33b58f286a35653c3feee0587f17515401213
Amit Kakkar <amit.kakkar24@gmail.com>
2021-05-17 11:23:47 -0400

    sln, matrix and great search BS

---
+++ Matrix/search-a-2d-matrix.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+    //Integers in each row are sorted from left to right.
+    //The first integer of each row is greater than the last integer of the previous row.
+namespace DataStructure.Matrix
+{
+    class search_a_2d_matrix
+    {
+        public bool SearchMatrix(int[][] matrix, int target)
+        {
+            int lastNum = 0;
+            bool isFound = false;
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                if (i != 0 && matrix[i][0] <= lastNum)
+                {
+                    return false;
+                }
+                int max = matrix[i][0];
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    if (max > matrix[i][j])
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        max = matrix[i][j];
+                    }
+                    lastNum = matrix[i][j];
+                    if (matrix[i][j] == target)
+                        isFound = true;
+                }
+            }
+            return isFound;
+        }
+    }
+}
---
+++ Matrix/transpose-matrix.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Given a 2D integer array matrix, return the transpose of matrix.
+//The transpose of a matrix is the matrix flipped over its main diagonal, switching the matrix's row and column indices.
+namespace DataStructure.Matrix
+{
+    //Given a 2D integer array matrix, return the transpose of matrix.
+    //The transpose of a matrix is the matrix flipped over its main diagonal, switching the matrix's row and column indices.
+    class transpose_matrix
+    {
+        public int[][] Transpose(int[][] matrix)
+        {
+            int rows = matrix.Length;
+            int cols = matrix[0].Length;
+            int[][] transposed = new int[cols][];
+            for (int i = 0; i < cols; i++)
+            {
+                transposed[i] = new int[rows];
+            }
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    transposed[j][i] = matrix[i][j];
+                }
+            }
+            return transposed;
+        }
+    }
+}
